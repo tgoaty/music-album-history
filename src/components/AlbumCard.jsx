a7be4500d8d0364b2f2a1e3558c63b4d4f08f911
Nikolay Pushkin <adwedelf@gmail.com>
2024-10-19 01:02:48 +0700

    2

--- src/components/AlbumCard.jsx
+++ src/components/AlbumCard.jsx
@@ -6,12 +6,10 @@ const AlbumCard = ({album}) => {
       <h4 className="author">{album.author}</h4>
       {album.tracks.map((track, index) => (
         <div className="track--wrapper" key={index}>
-          <a className="track--title" href={track.url}>{track.title}</a>
-          {track.feats.map((feat, index) => (
-            <p className="feat" key={index}>
-              {feat}
-            </p>
-          ))}
+          <p className="track--position">{track.position}</p>
+          <div className="track--info"><a className="track--title" href={track.url}>{track.title}</a>
+            {track.feats.length !== 0 && <p className="feat">ft. {track.feats.join(', ')}</p>}
+          </div>
         </div>
       ))}
     </div>
