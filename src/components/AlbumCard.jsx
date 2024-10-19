cff1c93b323b6d0dcd9222e625501cde51714e92
Nikolay Pushkin <adwedelf@gmail.com>
2024-10-19 17:52:26 +0700

    4

--- src/components/AlbumCard.jsx
+++ src/components/AlbumCard.jsx
@@ -1,15 +1,27 @@
-import {useState} from "react";
+import {useRef, useState} from "react";
 
 const AlbumCard = ({album}) => {
   const [onlyFavorite, setOnlyFavorite] = useState(false);
+  const albumCardRef = useRef(null);
+
+  const handleClick = () => {
+    setOnlyFavorite(!onlyFavorite);
+
+    if (albumCardRef.current && !onlyFavorite) {
+      albumCardRef.current.scrollIntoView({
+        behavior: 'smooth',
+        block: "start",
+      });
+    }
+  }
 
   return (
-    <div className="album--card">
+    <div className="album--card" ref={albumCardRef}>
       <h2 className="releaseDate">{album.releaseDate}</h2>
-      <img onClick={() => setOnlyFavorite(!onlyFavorite)} className="image" src={album.image} alt={album.title}/>
+      <img onClick={handleClick} className="image" src={album.image} alt={album.title}/>
       <div className="album--info">
         <h2 className="title">{album.title}</h2>
-        <h4 className="author">{album.author}</h4>
+        <h4 className="author">{album.artist}</h4>
         {album.tracks.map((track, index) => (
           <div
             className={`track--wrapper ${track.favorite || onlyFavorite ? 'track--show' : 'track--hide'}`}
