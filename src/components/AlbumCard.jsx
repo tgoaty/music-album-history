452a99d956aa847727ebfde2226e0362af94a5e2
Nikolay Pushkin <adwedelf@gmail.com>
2024-10-19 18:08:20 +0700

    5

--- src/components/AlbumCard.jsx
+++ src/components/AlbumCard.jsx
@@ -1,24 +1,61 @@
-import {useRef, useState} from "react";
+import React, { useRef, useState, useEffect } from "react";
+import ColorThief from 'colorthief';
 
-const AlbumCard = ({album}) => {
+const AlbumCard = ({ album }) => {
   const [onlyFavorite, setOnlyFavorite] = useState(false);
   const albumCardRef = useRef(null);
+  const imageRef = useRef(null);
+  const [gradient, setGradient] = useState('');
 
+  // Функция для получения цвета изображения
+  const getColorsFromImage = () => {
+    const colorThief = new ColorThief();
+    if (imageRef.current && imageRef.current.complete) {
+      const colors = colorThief.getPalette(imageRef.current, 2); // Получаем 2 доминирующих цвета
+      const color1 = `rgb(${colors[0].join(',')})`; // Первый цвет
+      const color2 = `rgb(${colors[1].join(',')})`; // Второй цвет
+      setGradient(`linear-gradient(135deg, ${color1}, ${color2})`);
+    }
+  };
+
+  // useEffect для получения цветов после загрузки изображения
+  useEffect(() => {
+    if (imageRef.current) {
+      if (imageRef.current.complete) {
+        getColorsFromImage(); // Если изображение уже загружено
+      } else {
+        imageRef.current.addEventListener('load', getColorsFromImage); // Если изображение загружается
+      }
+    }
+    return () => {
+      if (imageRef.current) {
+        imageRef.current.removeEventListener('load', getColorsFromImage); // Чистим обработчик при демонтировании
+      }
+    };
+  }, []);
+
+  // Обработчик клика для переключения favorite и прокрутки
   const handleClick = () => {
     setOnlyFavorite(!onlyFavorite);
-
     if (albumCardRef.current && !onlyFavorite) {
       albumCardRef.current.scrollIntoView({
         behavior: 'smooth',
         block: "start",
       });
     }
-  }
+  };
 
   return (
-    <div className="album--card" ref={albumCardRef}>
+    <div className="album--card" ref={albumCardRef} style={{ background: gradient }}>
       <h2 className="releaseDate">{album.releaseDate}</h2>
-      <img onClick={handleClick} className="image" src={album.image} alt={album.title}/>
+      <img
+        ref={imageRef}
+        onClick={handleClick}
+        className="image"
+        src={album.image}
+        alt={album.title}
+        crossOrigin="anonymous" // Важно для получения цветов с изображений из внешних источников
+      />
       <div className="album--info">
         <h2 className="title">{album.title}</h2>
         <h4 className="author">{album.artist}</h4>
@@ -33,7 +70,6 @@ const AlbumCard = ({album}) => {
               {track.feats.length !== 0 && <p className="feat">ft. {track.feats.join(', ')}</p>}
             </div>
           </div>
-
         ))}
       </div>
     </div>
